9b57026745dc45e92ba861399c657c837f5f6f0e
Bence A. Tóth <tothab@gmail.com>
2021-12-17 21:14:13 +0100

    Minor changes

--- sierpinsky.js
+++ sierpinsky.js
@@ -1,8 +1,10 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const canvasSize = Math.min(window.innerWidth, window.innerHeight);
+
+canvas.width = canvasSize;
+canvas.height = canvasSize;
 
 const drawDot = ({ x, y }) => {
   ctx.beginPath();
@@ -12,9 +14,9 @@ const drawDot = ({ x, y }) => {
 
 drawDot(100, 100);
 
-const a = { x: canvas.width * 0.5, y: canvas.height * 0.1 };
-const b = { x: canvas.width * 0.2, y: canvas.height * 0.9 };
-const c = { x: canvas.width * 0.8, y: canvas.height * 0.9 };
+const a = { x: canvasSize * 0.5, y: canvasSize * 0.1 };
+const b = { x: canvasSize * 0.2, y: canvasSize * 0.9 };
+const c = { x: canvasSize * 0.8, y: canvasSize * 0.9 };
 
 ctx.fillStyle = "red";
 drawDot(a);
@@ -22,8 +24,8 @@ drawDot(b);
 drawDot(c);
 
 let point = {
-  x: Math.random() * canvas.width,
-  y: Math.random() * canvas.height,
+  x: Math.random() * canvasSize,
+  y: Math.random() * canvasSize,
 };
 
 ctx.fillStyle = "blue";
